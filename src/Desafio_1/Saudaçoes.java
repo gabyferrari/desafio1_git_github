4e9033c0610749e83b3dbfeab7197ce14a3fc383
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2024-09-16 18:17:18 -0300

    Bom dia

---
+++ src/Desafio_1/Saudaçoes.java
@@ -0,0 +1,13 @@
+package Desafio_1;
+
+public class Saudaçoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Bom dia");
+		
+
+	}
+
+}
